0e10e2d387d228257a0cb00311d377927ef9629f
Jimmy McNamara <jmcnamara44@gmail.com>
2018-05-25 14:03:04 -0700

    pass the tests allowing the user to see how old they are using different planets years as a reference point for their age.

--- spec/age-spec.js
+++ spec/age-spec.js
@@ -1,10 +1,33 @@
 import { Age } from "./../src/age.js"
 
 describe("Age", function() {
+  let reusableTodaysDate;
+  let reusableTestBirthday;
+  let reusableTestAge;
+  beforeEach(function() {
+    reusableTodaysDate = new Date(2018, 4, 25);
+    reusableTestBirthday = new Date(2000, 0, 1);
+    reusableTestAge = new Age(reusableTestBirthday, reusableTodaysDate);
+  });
   it("should test if age calculation works", function() {
     let todaysDate = new Date(2018, 4, 25);
     let testBirthday = new Date(2018, 3, 14);
     let testAge = new Age(testBirthday, todaysDate);
     expect(testAge.ageInSeconds()).toEqual(3542400);
+  });
+  it("should test if age calculation for Mercury works", function() {
+    expect(reusableTestAge.mercuryAgeYears()).toEqual(77);
+  });
+  it("should test if age calculation for Venus works", function() {
+    expect(reusableTestAge.venusAgeYears()).toEqual(30);
+  });
+  it("should test if age calculation for Earth works", function() {
+    expect(reusableTestAge.earthAgeYears()).toEqual(18);
+  });
+  it("should test if age calculation for Mars works", function() {
+    expect(reusableTestAge.marsAgeYears()).toEqual(10);
+  });
+  it("should test if age calculation for Jupiter works", function() {
+    expect(reusableTestAge.jupiterAgeYears()).toEqual(2);
   });
 });
--- src/age.js
+++ src/age.js
@@ -9,4 +9,38 @@ export class Age {
     let secondsAge = ((this.compareToDate - this.birthday)/1000);
     return secondsAge;
   }
+  mercuryAgeYears()
+  {
+    let secondsAge = ((this.compareToDate - this.birthday)/1000);
+    let earthAge = (secondsAge/86400/365);
+    let mercuryAge = Math.round(earthAge / .24);
+    return mercuryAge;
+  }
+  venusAgeYears()
+  {
+    let secondsAge = ((this.compareToDate - this.birthday)/1000);
+    let earthAge = (secondsAge/86400/365);
+    let venusAge = Math.round(earthAge / .62);
+    return venusAge;
+  }
+  earthAgeYears()
+  {
+    let secondsAge = ((this.compareToDate - this.birthday)/1000);
+    let earthAge = Math.round(secondsAge/86400/365);
+    return earthAge;
+  }
+  marsAgeYears()
+  {
+    let secondsAge = ((this.compareToDate - this.birthday)/1000);
+    let earthAge = (secondsAge/86400/365);
+    let marsAge =Math.round( earthAge / 1.88);
+    return marsAge;
+  }
+  jupiterAgeYears()
+  {
+    let secondsAge = ((this.compareToDate - this.birthday)/1000);
+    let earthAge = (secondsAge/86400/365);
+    let jupiterAge = Math.round(earthAge / 11.87);
+    return jupiterAge;
+  }
 }
